ea6c92d07eccf25abea99085410e1c3999c348c4
Dino Morelli <dino@ui3.info>
2024-09-25 08:59:35 -0400

    Library working
    We now know how to make wreq NOT throw exceptions for any non-2XX
    response. This code can also be used directly as a library.

--- src/lib/Network/Wreq/Extra.hs
+++ src/lib/Network/Wreq/Extra.hs
@@ -1,4 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.Wreq.Extra
+  ( getAny
+  , postAny
+  )
   where
 
+import Data.ByteString.Lazy ( ByteString )
+import Lens.Micro ( set )
+import Network.Wreq
+  ( Options, Response
+  , checkResponse, defaults, getWith, postWith
+  )
+import Network.Wreq.Types ( Postable )
 
+
+lenientResponseOptions :: Options
+lenientResponseOptions = set checkResponse (Just $ \_ _ -> return ()) defaults
+
+
+getAny :: String -> IO (Response ByteString)
+getAny = getWith lenientResponseOptions
+
+
+postAny :: Postable a => String -> a -> IO (Response ByteString)
+postAny = postWith lenientResponseOptions
--- src/test/Main.hs
+++ src/test/Main.hs
@@ -1,3 +1,11 @@
+{-# LANGUAGE ImportQualifiedPost, OverloadedStrings #-}
+
+import Data.ByteString.Char8 qualified as BS
+import Data.Char ( toUpper )
+import Data.String.Conv ( toS )
+import Lens.Micro ( (^.) )
+import Network.Wreq ( responseStatus, statusCode, statusMessage )
+import Network.Wreq.Extra ( getAny )
 import Test.Tasty
 import Test.Tasty.HUnit
 
@@ -8,8 +16,31 @@ main = defaultMain $ testGroup " tests"
   ]
 
 
+testServerUrl :: String
+testServerUrl = "http://httpstat.us/"
+-- testServerUrl = "http://httpbin.org/status/"
+
+
+testEndpoint :: Int -> BS.ByteString -> TestTree
+testEndpoint code msg = testCase (show code <> " " <> toS msg) $ do
+  response <- getAny $ testServerUrl <> (show code)
+  code @=? response ^. responseStatus . statusCode
+  (BS.map toUpper msg) @=? (BS.map toUpper $ response ^. responseStatus . statusMessage)
+
+
+-- NOTE: 3XX codes are for redirection and wreq follows the Location header to
+-- the new URI, resulting in a 200 OK. So those codes are not a good test here.
+
 unitTests :: TestTree
 unitTests = testGroup "unit tests"
-  [ testCase "True is True" $
-      True @=? True
+  [ testEndpoint 200 "OK"
+  , testEndpoint 202 "Accepted"
+  , testEndpoint 226 "IM Used"
+  , testEndpoint 400 "Bad Request"
+  , testEndpoint 404 "Not Found"
+  , testEndpoint 407 "Proxy Authentication Required"
+  , testEndpoint 418 "I'm a teapot"
+  , testEndpoint 500 "Internal Server Error"
+  , testEndpoint 503 "Service Unavailable"
+  , testEndpoint 507 "Insufficient Storage"
   ]
