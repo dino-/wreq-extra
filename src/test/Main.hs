e553a3cb67455e3751a0aed2378d0e3b7b7633c3
Dino Morelli <dino@ui3.info>
2024-09-25 09:32:22 -0400

    Added POST tests
    This work also consolidated the test code for better sharing.
    
    Also switched to using httpbin.org/status/ for creating the responses
    which seems to be a more reliable source. The status messages that come
    back from this server are upper-case, so made a change to make the test
    msg comparison case-insensitive (which is what a lot of the WWW is like)

--- src/test/Main.hs
+++ src/test/Main.hs
@@ -1,46 +1,73 @@
 {-# LANGUAGE ImportQualifiedPost, OverloadedStrings #-}
 
 import Data.ByteString.Char8 qualified as BS
+import Data.ByteString.Lazy qualified as BL
 import Data.Char ( toUpper )
 import Data.String.Conv ( toS )
 import Lens.Micro ( (^.) )
+import Network.HTTP.Client.Internal ( Response )
 import Network.Wreq ( responseStatus, statusCode, statusMessage )
-import Network.Wreq.Extra ( getAny )
+import Network.Wreq.Extra ( getAny, postAny )
 import Test.Tasty
 import Test.Tasty.HUnit
 
 
 main :: IO ()
 main = defaultMain $ testGroup " tests"
-  [ unitTests
+  [ getTests
+  , postTests
   ]
-
-
-testServerUrl :: String
-testServerUrl = "http://httpstat.us/"
--- testServerUrl = "http://httpbin.org/status/"
-
-
-testEndpoint :: Int -> BS.ByteString -> TestTree
-testEndpoint code msg = testCase (show code <> " " <> toS msg) $ do
-  response <- getAny $ testServerUrl <> (show code)
-  code @=? response ^. responseStatus . statusCode
-  (BS.map toUpper msg) @=? (BS.map toUpper $ response ^. responseStatus . statusMessage)
 
 
 -- NOTE: 3XX codes are for redirection and wreq follows the Location header to
 -- the new URI, resulting in a 200 OK. So those codes are not a good test here.
 
-unitTests :: TestTree
-unitTests = testGroup "unit tests"
-  [ testEndpoint 200 "OK"
-  , testEndpoint 202 "Accepted"
-  , testEndpoint 226 "IM Used"
-  , testEndpoint 400 "Bad Request"
-  , testEndpoint 404 "Not Found"
-  , testEndpoint 407 "Proxy Authentication Required"
-  , testEndpoint 418 "I'm a teapot"
-  , testEndpoint 500 "Internal Server Error"
-  , testEndpoint 503 "Service Unavailable"
-  , testEndpoint 507 "Insufficient Storage"
+getTests :: TestTree
+getTests = testGroup "GET tests"
+  [ testGet 200 "OK"
+  , testGet 202 "Accepted"
+  , testGet 226 "IM Used"
+  , testGet 400 "Bad Request"
+  , testGet 404 "Not Found"
+  , testGet 407 "Proxy Authentication Required"
+  , testGet 418 "I'm a teapot"
+  , testGet 500 "Internal Server Error"
+  , testGet 503 "Service Unavailable"
+  , testGet 507 "Insufficient Storage"
   ]
+
+
+postTests :: TestTree
+postTests = testGroup "POST tests"
+  [ testPost 200 "OK"
+  , testPost 202 "Accepted"
+  , testPost 226 "IM Used"
+  , testPost 400 "Bad Request"
+  , testPost 404 "Not Found"
+  , testPost 407 "Proxy Authentication Required"
+  , testPost 418 "I'm a teapot"
+  , testPost 500 "Internal Server Error"
+  , testPost 503 "Service Unavailable"
+  , testPost 507 "Insufficient Storage"
+  ]
+
+
+testServerUrl :: String
+testServerUrl = "http://httpbin.org/status/"
+
+
+evaluateResponse :: Int -> BS.ByteString -> (String -> IO (Response BL.ByteString)) -> Assertion
+evaluateResponse code msg requestFunc = do
+  response <- requestFunc $ testServerUrl <> (show code)
+  code @=? response ^. responseStatus . statusCode
+  (BS.map toUpper msg) @=? (BS.map toUpper $ response ^. responseStatus . statusMessage)
+
+
+testGet :: Int -> BS.ByteString -> TestTree
+testGet code msg = testCase (show code <> " " <> toS msg) $
+  evaluateResponse code msg getAny
+
+
+testPost :: Int -> BS.ByteString -> TestTree
+testPost code msg = testCase (show code <> " " <> toS msg) $
+  evaluateResponse code msg $ flip postAny ("foobar" :: BS.ByteString)
